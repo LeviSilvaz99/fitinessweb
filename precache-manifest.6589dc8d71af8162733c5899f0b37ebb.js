6f498c9774381e60b304b0591f51fdd6bc53c685
André Levi Oliveira Silva <57275766+LeviSilvaz99@users.noreply.github.com>
2020-04-23 20:25:53 -0300

    Updates

---
+++ precache-manifest.6589dc8d71af8162733c5899f0b37ebb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27feec80160bf73f5a4cb0b915b53846",
+    "url": "/fitinessweb/index.html"
+  },
+  {
+    "revision": "717230a517e5ba404130",
+    "url": "/fitinessweb/static/css/main.ab6fe7fb.chunk.css"
+  },
+  {
+    "revision": "0f649c65b7ae9f82a677",
+    "url": "/fitinessweb/static/js/2.e71eaf6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fitinessweb/static/js/2.e71eaf6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717230a517e5ba404130",
+    "url": "/fitinessweb/static/js/main.8627f42a.chunk.js"
+  },
+  {
+    "revision": "015f02724470d9706f79",
+    "url": "/fitinessweb/static/js/runtime-main.defeae7e.js"
+  },
+  {
+    "revision": "2ff2fff5c0f6b8fc98dd99ef45086598",
+    "url": "/fitinessweb/static/media/bg.2ff2fff5.mp4"
+  },
+  {
+    "revision": "6d12206fef699c3cb783709a4335be15",
+    "url": "/fitinessweb/static/media/man.6d12206f.png"
+  }
+]);
